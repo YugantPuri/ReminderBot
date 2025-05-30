e99c62f0f123095d469a4438dd37fdb25d5725ce
YugantPuri <081bct096.yugant@pcampus.edu.np>
2025-05-30 22:08:35 +0545

    initial

---
+++ main.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+print(api_key)
+
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True  # Needed to read messages
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello! I'm your bot.")
+
+bot.run(api_key)
